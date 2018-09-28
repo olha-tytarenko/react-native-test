0773e1bb8bf521dc82e7790f3a8729557f20a8c8
Olha Tytarenko <titarenko.olga.1997@gmail.com>
2018-09-28 17:21:40 +0300

    -

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { NativeRouter, Route, Link } from 'react-router-native'
 import {ThemeProvider } from 'styled-components';
 import {Header} from './src/components/header/header';
 import {GistsScreen, RepositoriesScreen} from './src/screens';
-import {theme} from './theme';
+import {theme} from './styles/theme';
 import * as routes from './constants/routes';
 
 type Props = {};
@@ -14,20 +14,20 @@ export default class App extends Component<Props> {
       <ThemeProvider theme={theme}>
       <NativeRouter>
         <View>
-            <Header title="Main page" />
-            <Link
-              to={routes.GISTS}
-            >
-              <Text>Gists</Text>
-            </Link>
-            <Link
-              to={routes.REPOSITORIES}
-            >
-              <Text>Repositories</Text>
-            </Link>
+          <Header title="Main page" />
+          <Link
+            to={routes.GISTS}
+          >
+            <Text>Gists</Text>
+          </Link>
+          <Link
+            to={routes.REPOSITORIES}
+          >
+            <Text>Repositories</Text>
+          </Link>
+          <Route path={routes.GISTS} component={GistsScreen}/>
+          <Route path={routes.REPOSITORIES} component={RepositoriesScreen}/>
         </View>
-        <Route path={routes.GISTS} component={GistsScreen}/>
-        <Route path={routes.REPOSITORIES} component={RepositoriesScreen}/>
       </NativeRouter>
       </ThemeProvider>
     );
